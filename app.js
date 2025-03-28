125277e7ba913b4d10e6df7ea23134a15bb40a36
vaishnavi lanjewar <136914474+VAISHNAVISTUDENT@users.noreply.github.com>
2025-03-29 00:05:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.post('/validate', (req, res) => {
 app.all('*', (req, res) => {
     res.send("No such Page")
   });
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
